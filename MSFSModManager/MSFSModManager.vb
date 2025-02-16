14b0a5e6ba61c0b63dc106e785ae8bd2b35ee7d2
Claudio Persico <clapersico@gmail.com>
2025-02-16 21:33:26 +0000

    Disattivazione mod

--- MSFSModManager/MSFSModManager.vb
+++ MSFSModManager/MSFSModManager.vb
@@ -1,6 +1,13 @@
-﻿Public Class MSFSModManager
+﻿Imports System.IO
+Imports System.Threading
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class MSFSModManager
     Public Const REG_KEY_BASE As String = "SOFTWARE\\PersicoProjects\\MSFSModManager\\"
     Public Const REG_KEY_DIR_MOD As String = REG_KEY_BASE + "dir"
+
+    Private cartellaMod As String
+    Private cartellaMSFS As String
 
     Private Sub OpzioniToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpzioniToolStripMenuItem.Click
         Me.Enabled = False
@@ -8,18 +15,12 @@
     End Sub
 
     Private Sub MSFSModManager_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim cartellaMod = RegistroSistema.leggiValoreChiave(REG_KEY_DIR_MOD, "mod")
-        Dim cartellaMSFS = RegistroSistema.leggiValoreChiave(REG_KEY_DIR_MOD, "msfs")
+        cartellaMod = RegistroSistema.leggiValoreChiave(REG_KEY_DIR_MOD, "mod")
+        cartellaMSFS = RegistroSistema.leggiValoreChiave(REG_KEY_DIR_MOD, "msfs")
 
         caricamentoAddon(ModList, cartellaMod)
         caricamentoAddon(ActiveModList, cartellaMSFS)
-
-        If ModList.Items.Count = 0 And ActiveModList.Items.Count = 0 Then
-            attivaModBt.Enabled = False
-            attivaTutteModBt.Enabled = False
-            disattivaModBt.Enabled = False
-            disattivaTutteModBt.Enabled = False
-        End If
+        attivaDisattivaBt()
     End Sub
 
     Private Sub attivaModBt_Click(sender As Object, e As EventArgs) Handles attivaModBt.Click
@@ -38,6 +39,33 @@
 
                 control.Items.Add(nome)
             Next
+        End If
+    End Sub
+
+    Private Sub disattivaModBt_Click(sender As Object, e As EventArgs) Handles disattivaModBt.Click
+        If ActiveModList.SelectedIndex > -1 Then
+            My.Computer.FileSystem.MoveDirectory(cartellaMSFS + ActiveModList.SelectedItem, cartellaMod + ActiveModList.SelectedItem)
+            ModList.Items.Add(ActiveModList.SelectedItem)
+            ActiveModList.Items.Remove(ActiveModList.SelectedItem)
+
+            attivaDisattivaBt()
+        End If
+    End Sub
+
+    Private Sub attivaDisattivaBt()
+        attivaModBt.Enabled = False
+        attivaModBt.Enabled = False
+        disattivaModBt.Enabled = False
+        disattivaTutteModBt.Enabled = False
+
+        If ModList.Items.Count > 0 Then
+            attivaModBt.Enabled = True
+            attivaTutteModBt.Enabled = True
+        End If
+
+        If ActiveModList.Items.Count > 0 Then
+            disattivaModBt.Enabled = True
+            disattivaTutteModBt.Enabled = True
         End If
     End Sub
 End Class
